67932132b31bda1fce025472d10698c8b665542d
RenishAndani <renish.andani@prominentpixel.com>
2026-08-27 17:29:03 +0530

    add day2 about debug

---
+++ Day2/dist/app.js
@@ -0,0 +1,7 @@
+"use strict";
+function add(n1, n2) {
+  return n1 + n2;
+}
+const sum = add(10, 5);
+console.log(sum);
+//# sourceMappingURL=app.js.map
---
+++ Day2/src/app.ts
@@ -0,0 +1,7 @@
+function add(n1: number, n2: number): number {
+  return n1 + n2;
+}
+
+const sum: number = add(10, 5);
+
+console.log(sum);
